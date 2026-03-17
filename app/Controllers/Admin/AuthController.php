012c5746721038b22f881860119b84c77a6c80db
AndreJudiao <andrejudiao@gmail.com>
2026-03-17 15:00:19 +0100

    Ajustando o login, se ele ja estiver logado nao pode voltar a tela de login

--- app/Controllers/Admin/AuthController.php
+++ app/Controllers/Admin/AuthController.php
@@ -16,6 +16,17 @@ class AuthController extends Controller
 
     public function loginForm()
     {
+         // Se já estiver logado, redireciona para o painel
+        if (!empty($_SESSION['admin_logged'])) {
+            header('Location: /admin');
+            exit;
+        }
+
+        // Se já estiver logado, redireciona para o painel
+        if (!empty($_SESSION['admin_logged'])) {
+            header('Location: /admin');
+            exit;
+        }
         $this->view('deashboad/login', [
             'message' => 'Olá Mundo com Twig'
         ]);
